b4f4e23898e15144dbedd556404f4447a9c847be
Weston Dransfield <wdrans@gmail.com>
2022-10-01 16:23:50 -0600

    update db name

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,14 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db('courtneyDatabase').collection('contacts').find();
+  const result = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
@@ -13,11 +17,11 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db('courtneyDatabase')
-    .collection('contacts')
+    .db("cs_341_projects")
+    .collection("contacts")
     .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
